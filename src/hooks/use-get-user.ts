395c09faac87a3dfca92a3c3411185b4b8a01dfc
t-yaginuma <toru45s@gmail.com>
2024-09-16 17:18:59 +0900

    update hooks

--- src/hooks/use-get-user.ts
+++ src/hooks/use-get-user.ts
@@ -3,8 +3,10 @@ import { createClient } from "@/libs/supabase/server";
 export default async function useGetUser() {
   const supabase = createClient();
   const { data, error } = await supabase.auth.getUser();
-  console.log("getUser======");
-  console.log(data);
-  const isAuthenticated = !!data.user ? true : false;
-  return { isAuthenticated };
+  // console.log("getUser======");
+  // console.log(data);
+
+  const loginUserId = data.user?.id;
+
+  return { loginUserId };
 }
